8b3276cb905e9323852b6cabc4038aeffca00e47
Marcio Rogerio Nizzola <marcionizzola@gmail.com>
2022-05-03 22:34:38 -0300

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ObraControl',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
